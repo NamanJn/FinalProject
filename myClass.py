45be02c068c70aa3830cec370917afba9e7fa009
naman <nj1411@ic.ac.uk>
2015-07-13 13:28:54 +0100

    now a user can specifiy a tube number for an ROI

--- myClass.py
+++ myClass.py
@@ -24,8 +24,7 @@ class Tracker(object):
         """
         returns moments (center) of contours
         """
-        self.frame = frame
-        if self.counter %100 == 0: pdb.set_trace()
+        self.frame = frame.copy()
 
 	cv2.cvtColor(frame,code=cv2.COLOR_BGR2GRAY,dst=self.gray)
         gray_float = self.gray.astype("float32") 
@@ -37,21 +36,28 @@ class Tracker(object):
 	cv2.imshow("diff", self.diff)
 
 
+        # drawing the binary threshold image
         cv2.threshold(src=self.diff,thresh=20,
                 maxval=255,
                 type=cv2.THRESH_BINARY,
                 dst=self.binary)
-
-         # finding the contours 
         contour = self.binary.copy()
 	cv2.imshow("binary", self.binary)
+
+
+        # finding the contours 
         contourL, hierarchy = cv2.findContours(image=contour,
                     #mode=cv2.RETR_TREE,
                     mode=cv2.RETR_EXTERNAL,
                     method=cv2.CHAIN_APPROX_SIMPLE)
-
 	cv2.imshow("contour", contour)
-        print len(contourL)
+        print "Length of all contours ",len(contourL)
+
+
+        # drawing all contours 
+        allContourFrame = self.frame.copy()
+        cv2.drawContours(allContourFrame,contourL,-1,(255,255,0),-1)
+        cv2.imshow("no filtering - all contours shown", allContourFrame)
 
         # getting big contours  
         bigcontours = [i for i in contourL if cv2.contourArea(i)>50]
@@ -59,15 +65,23 @@ class Tracker(object):
         # This block is to prevent the losing of the contour
         self.counter +=1
         print self.counter
-
         if len(bigcontours) == 1:
             self.previousContour = bigcontours[:]
         elif len(bigcontours) == 0 and self.previousContour != ["test"]:
             bigcontours = self.previousContour[:]
 
-        if self.counter> 100:
-            
-            # getting the most squarish looking contour
+        # drawing the big contours
+        bigContourFrame = frame.copy() 
+        cv2.drawContours(bigContourFrame, bigcontours,-1,(255,255,0),1)
+	cv2.imshow("1st filter step - big contours", bigContourFrame)
+
+        # getting most squarish looking contour
+        # no need for this step if there is only 1 contour.
+        # unccoment line below if you want to have the length of the big 
+        #if self.counter> 100 and len(bigcontours) > 1:
+
+        if self.counter> 100 :
+
             squarishcontourL = self.getSquarishContour(bigcontours,draw=True)
 
             masked = np.zeros(self.gray.shape,np.uint8)     
@@ -78,8 +92,7 @@ class Tracker(object):
 
             cv2.imshow("mask",masked) 
 
-
-            frame_with_square_contour= frame.copy()
+            frame_with_square_contour= self.frame.copy()
             cv2.drawContours(frame_with_square_contour, squarishcontourL,-1,(255,255,0),1)
 	    cv2.imshow("2nd filter step - squarish", frame_with_square_contour)
 
@@ -94,14 +107,11 @@ class Tracker(object):
             positions.append([cx,cy])   
         print positions
 
-        cv2.drawContours(frame, bigcontours,-1,(255,255,0),1)
-
-
-	cv2.imshow("img", frame)
 
 
 	cv2.waitKey(10)
 
+        if self.counter %100 == 0 and self.counter> 1: pdb.set_trace()
         return positions 
 
     def getSquarishContour(self,contourL,draw=False):
@@ -116,7 +126,8 @@ class Tracker(object):
                 cv2.rectangle(frameCopy,(x,y),(x+w,y+h),(0,255,0),1)
             aspect_ratios.append(abs(float(w)/h -1))
 
-        cv2.imshow("squares",frameCopy)
+        if draw:
+            cv2.imshow("squares",frameCopy)
         min_index, min_value = min(enumerate(aspect_ratios), key=operator.itemgetter(1))
         squarishcontourL = [contourL.pop(min_index)]
         return squarishcontourL 
--- testclass.py
+++ testclass.py
@@ -7,9 +7,17 @@ from myClass import Tracker
 ee = execfile
 
 counter = 0
+
+# you need to specify this number 
+tube_number = 9
+
+# tube 9, 
+# 1. fly sleeps for time.
+# 2. Fly contour gets split into half because the torso is below threshold 
+
 tube_height = 64
 tube_length = 550
-tube_y = 430
+tube_y = 160 + 65*(tube_number-1)
 tube_x = 50
 
 # reading the video
@@ -24,9 +32,9 @@ tracker = Tracker(frame)
 while True:
 
 	ret, frame = cap.read()
-        frame = frame[tube_y:tube_y+tube_height,tube_x:tube_x+tube_length]
+        frameROI = frame[tube_y:tube_y+tube_height,tube_x:tube_x+tube_length]
 
-        positions = tracker.apply(frame)
+        positions = tracker.apply(frameROI)
 
 	print counter 
 	counter +=1
