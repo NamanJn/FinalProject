fac8d552d8bb055b410c90914eeee0db9970510f
naman <nj1411@ic.ac.uk>
2015-08-03 12:21:03 +0100

    now can manually annotate all videos at once

--- validate2.py
+++ validate2.py
@@ -1,7 +1,7 @@
 import inspect 
 import cv2
 import pdb
-
+import os
 # This script's aim is to get input from a user
 # whether the tracking system switched the identity or no
 # Ths userinput will just be "yes" or "no".
@@ -13,76 +13,82 @@ from twoflies import Tracker
 ee = execfile
 
 counter = 0
-
-# reading the video
-collisionFrame = 916 
-cap = cv2.VideoCapture("collision_vids/collision%s_withcontours.mp4" % collisionFrame)
-
-# initialising the tracker class
-print cap.get(3)
-print cap.get(4)
-print cap.get(5)
+videoDirS = "collision_vids"
+videosL = os.listdir(videoDirS)
 
 
-ret, frame = cap.read()
-#frameROI = frame[ tube_y:tube_y+tube_height, tube_x:tube_x+tube_length ]
-#tracker = Tracker(frame, num_of_flies=2)
+for videoS in videosL:
+    # reading the video
+    #collisionFrame = 916 
+    #cap = cv2.VideoCapture("collision_vids/collision%s_withcontours.mp4" % collisionFrame)
+    pdb.set_trace()    
+    cap = cv2.VideoCapture(os.path.join(videoDirS,videoS))
 
-# listener function
-#positionsL = []
-#def draw_circle(event,x,y,flags,param):
-#    global cv2,frame,positionsL
-#    print param
-#    if event == cv2.EVENT_LBUTTONUP and len(positionsL) < param:
-#        print x,y
-#        positionsL.append([x,y])
-#        print "Now positionsL is,", positionsL
-#
-## binding listener to the image
-#cv2.namedWindow('image')
-#cv2.setMouseCallback('image', draw_circle, param=2)
-
-# showing the first image and waiting for 2 clicks
-cv2.imshow("image",frame)
-while True:
-    aKey = cv2.waitKey(0) 
-    if aKey == ord("a"):
-        break
+    # initialising the tracker class
+    print cap.get(3)
+    print cap.get(4)
+    print cap.get(5)
 
 
-frame_shape = frame.shape
-write_video = False 
+    ret, frame = cap.read()
+    #frameROI = frame[ tube_y:tube_y+tube_height, tube_x:tube_x+tube_length ]
+    #tracker = Tracker(frame, num_of_flies=2)
 
-while True:
-        breakloop = False
+    # listener function
+    #positionsL = []
+    #def draw_circle(event,x,y,flags,param):
+    #    global cv2,frame,positionsL
+    #    print param
+    #    if event == cv2.EVENT_LBUTTONUP and len(positionsL) < param:
+    #        print x,y
+    #        positionsL.append([x,y])
+    #        print "Now positionsL is,", positionsL
+    #
+    ## binding listener to the image
+    #cv2.namedWindow('image')
+    #cv2.setMouseCallback('image', draw_circle, param=2)
 
-        cv2.waitKey(100)
-	ret, frame = cap.read()
-        
-        if not ret:
-            while True:
-                aKey = cv2.waitKey(0) 
-                if aKey == ord("a"):
-                    breakloop = True
-                    break
-                         
-        if breakloop:
+    # showing the first image and waiting for 2 clicks
+
+    cv2.imshow("image",frame)
+    while True:
+        aKey = cv2.waitKey(0) 
+        if aKey == ord("a"):
             break
 
-        cv2.imshow("image", frame)
-	counter += 1
-	print counter 
 
+    frame_shape = frame.shape
+    write_video = False 
 
-while True:
-    x = raw_input("Did identities switch? [y/n]")
-    if x == "y":
-        print "ok switched"
-        break
-    elif x == "n":
-        print "ok didn't switch"
-        break
-    else:
-        print "try again"
+    while True:
+            breakloop = False
 
+            cv2.waitKey(100)
+            ret, frame = cap.read()
+            
+            if not ret:
+                while True:
+                    aKey = cv2.waitKey(0) 
+                    if aKey == ord("a"):
+                        breakloop = True
+                        break
+                             
+            if breakloop:
+                break
 
+            cv2.imshow("image", frame)
+            counter += 1
+            print counter 
+
+    while True:
+        x = raw_input("Did identities switch? [y/n]")
+        if x == "y":
+            print "ok switched"
+            break
+        elif x == "n":
+            print "ok didn't switch"
+            break
+        else:
+            print "try again"
+
+    cap.release()
