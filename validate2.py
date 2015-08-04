d40a5d963f4ceed9f748111ff84c7d69ad96d8f0
naman <nj1411@ic.ac.uk>
2015-08-04 17:29:14 +0100

    User is able to play the previous video

--- validate2.py
+++ validate2.py
@@ -7,22 +7,20 @@ import os
 # Ths userinput will just be "yes" or "no".
 # If yes, then switch the identities downstream of the collision.
 # If no, then nothing is changed downstream. 
-
 from twoflies import Tracker
-
+import re
 ee = execfile
 
-
-videoDirS = "collision_vids"
-videosL = os.listdir(videoDirS)
 
 def playVideo(videoNameS):
 
     counter = 0
     cap = cv2.VideoCapture(os.path.join(videoDirS,videoS))
     ret, frame = cap.read()
-    cv2.imshow("image",frame)
+    cv2.imshow("image", frame)
     while True:
+        print "press a to continue"
+        print "playing video", videoNameS
         aKey = cv2.waitKey(0) 
         if aKey == ord("a"):
             break
@@ -39,8 +37,8 @@ def playVideo(videoNameS):
             
             if not ret:
                 while True:
-                    aKey = cv2.waitKey(0) 
-                    if aKey == ord("a"):
+                    #aKey = cv2.waitKey(0) 
+                    #if aKey == ord("a"):
                         breakloop = True
                         break
                              
@@ -50,11 +48,19 @@ def playVideo(videoNameS):
             cv2.imshow("image", frame)
             counter += 1
      
-for videoS in videosL:
+
+videoDirS = "collision_vids"
+videosL = os.listdir(videoDirS)
+videosL = sorted(videosL, key= lambda x: int(re.findall(r"collision(\d+)_", x)[0]))
+pdb.set_trace()
+
+video_number = 0
+#for videoS in videosL[::-1]:
+while video_number < len(videosL):
     # reading the video
     #collisionFrame = 916 
     #cap = cv2.VideoCapture("collision_vids/collision%s_withcontours.mp4" % collisionFrame)
-    pdb.set_trace()    
+    videoS = videosL[::-1][video_number]
     print "playing collision video:", videoS
     playVideo(os.path.join(videoDirS, videoS))
 
@@ -78,15 +84,22 @@ for videoS in videosL:
     # showing the first image and waiting for 2 clicks
 
     while True:
-        x = raw_input("Did identities switch? [y/n/r]: ")
+        x = raw_input("Did identities switch? [y/n/r/p]: ")
         if x == "y":
             print "ok switched"
+            video_number += 1
             break
         elif x == "n":
             print "ok didn't switch"
+            video_number += 1
             break
         elif x == "r":
-            print "playing again"
+            print "playing again chosen.... press a to continue"
+            playVideo(os.path.join(videoDirS, videoS))
+        elif x == "p":
+            print "playing previous video again chosen.... press a to continue"
+            video_number -= 1
+            videoS = videosL[::-1][video_number]
             playVideo(os.path.join(videoDirS, videoS))
         else:
             print "try again"
