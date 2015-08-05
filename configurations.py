d90fe62243f5d01c97af32d1121dedbbebf3fe53
naman <nj1411@ic.ac.uk>
2015-08-05 20:42:59 +0100

    added the global configurations file

---
+++ configurations.py
@@ -0,0 +1,20 @@
+
+
+# cv2.imshow image size increase (x-axis) 
+width_multiple_to_increase = 2
+
+# cv2.imshow image size increase (y-axis) 
+height_multiple_to_increase = 2
+
+
+
+# frames_per_second
+fps = 25.0
+
+# contour images directory
+contour_images_dir = "contour_imgs"
+
+
+# validation_results_directory
+validation_results_dir = "validation_results"
+
