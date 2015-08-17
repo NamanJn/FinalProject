3557891fa43462f76532de9cb62ee02adda0c2d8
naman <nj1411@ic.ac.uk>
2015-08-17 16:14:12 +0100

    added a class for the correction

--- correct_data.py
+++ correct_data.py
@@ -1,5 +1,5 @@
 
-from configurations import data_dir,corrected_data_dir,validation_results_dir,col_names
+from configurations import data_dir, corrected_data_dir, validation_results_dir, col_names
 import os
 import pandas as pd
 import inspect
@@ -9,11 +9,41 @@ ee = execfile
 poing = inspect.getabsfile(inspect.currentframe())
 
 
+class CorrectCollisions(object):
+    def __init__(self, data_file, validated_results, rle):
+        self.data_file = data_file
+        self.validated_results = validated_results
+        self.rle = rle
+        self.correctedData = None
+        self.yes_valuesL = self.validated_results[self.validated_results.icol(1) == "y"].values[:,0]
+
+    def getCorrectedData(self):
+        if self.correctedData == None:
+            self.correctedData = 5   # fix this.
+        return self.correctedData
+
+    def get_switched_frames(self):
+
+        valuesL = []
+        for i in range(0, len(self.yes_valuesL), 2):
+
+            startingBound = self.yes_valuesL[i]
+            upperBound = self.yes_valuesL[i+1]
+            print startingBound
+            print upperBound
+            valuesL += range(startingBound, upperBound)
+
+        return valuesL
+
+    def __repr__(self):
+        return "No. of rows: %s" % len(self.data_file)
+
 def flipFrameIdentities(dataFrame):
     dataFrameCopy = dataFrame.copy()
-    dataFrameCopy.iloc[0,2:] = dataFrame.iloc[1,2:]
-    dataFrameCopy.iloc[1,2:] = dataFrame.iloc[0,2:]
+    dataFrameCopy.iloc[0, 2:] = dataFrame.iloc[1, 2:]
+    dataFrameCopy.iloc[1, 2:] = dataFrame.iloc[0, 2:]
     return dataFrameCopy
+
 
 dataFileS = "data_shortcoll.csv"
 dataFilePathS = os.path.join(data_dir, dataFileS)
@@ -52,3 +82,5 @@ for i in range(0, len(yesValuesL), 2):
 
 # raw_data_copy is the final answer
 
+# trying class
+a = CorrectCollisions(raw_data, results, rle)
