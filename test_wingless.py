b44842a731e37feb8aef38af7c276f6cf157f155
naman <nj1411@ic.ac.uk>
2015-08-28 18:35:14 +0100

    now testing wingless video

---
+++ test_wingless.py
@@ -0,0 +1,8 @@
+
+import os
+import test2flies
+
+
+test2flies.runTracker('videos/wingless_1hour.mp4', 'wingless',
+                      tube_number=3, y_offset=154, tube_height=24,
+                      initial_alpha=0.2)
