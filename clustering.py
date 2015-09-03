f39be7f2ed8815530f6b59543ce1ddf1f9aa602f
naman <nj1411@ic.ac.uk>
2015-09-03 19:21:42 +0100

    Now have average features per fly per intercollision set of frames

--- clustering.py
+++ clustering.py
@@ -4,7 +4,8 @@ Usage:
         clustering.py <results_directory>
 
 """
-
+import test_rle
+import pdb
 from sklearn.cluster import KMeans
 import inspect
 import numpy as np
@@ -18,7 +19,7 @@ poing = inspect.getabsfile(inspect.currentframe())
 ee = execfile
 
 d = docopt.docopt(__doc__)
-results_directory = os.path.join(configurations.output_dir, ['<results_directory>'])
+results_directory = os.path.join(configurations.output_dir, d['<results_directory>'])
 print "This is the results directory", results_directory
 dataFilePathS = os.path.join(results_directory, data_file)
 
@@ -33,13 +34,24 @@ feature_set = np.array([[7.65235, 2.08],
                 [-1.5, -3.75]])
 
 
-
 generated = pd.read_csv(dataFilePathS, names=col_names)
+
 
 # getting non-collisions by filtering via the 'fly width' column
 nonCollisions = generated[generated.width != collision_value]
+nonCollisionsL = test_rle.getInterCollisionsFromDataFile('tube4')
+
+# this gets the average values of features per fly per set of intercollision frames
+featuresPerFlyPerInterCollision = []
+for i in nonCollisionsL:
+    fly1 = i[i[:, 1] == "fly1"]
+    fly2 = i[i[:, 1] == "fly2"]
+    fly1Average = np.average(fly1[:, 4:], axis=0)
+    fly2Average = np.average(fly2[:, 4:], axis=0)
+    featuresPerFlyPerInterCollision.append([fly1Average, fly2Average])
 
 
+sys.exit()
 feature_set = nonCollisions.iloc[:, 4:]
 
 km = KMeans(n_clusters=2)
