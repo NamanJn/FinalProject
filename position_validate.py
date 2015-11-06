b76d32f88d6aed21d6596c49f68ec9e6a80fef33
Naman Jain <nj1411@ic.ac.uk>
2015-11-06 11:37:22 +0000

    Moved lines to __main__ section

--- position_validate.py
+++ position_validate.py
@@ -7,31 +7,11 @@ Usage:
 
 import inspect
 import cv2
-import pdb
 import os
-
 import configurations
 import numpy as np
 import docopt
-ee = execfile
 
-d = docopt.docopt(__doc__)
-results_directory = os.path.join(configurations.output_dir, d['<results_directory>'])
-
-if not os.path.exists(results_directory):
-    raise EnvironmentError("The '%s' does not exist. It probably means you have not run the analysis yet" % results_directory )
-
-
-dir_path = os.path.join(results_directory, configurations.raw_imgs_dir)
-results_file = "position_validation.csv"
-
-
-validation_results_dir = os.path.join(results_directory, configurations.validation_results_dir)
-fileToWriteResults = os.path.join(validation_results_dir, results_file)
-
-# create directory if doesn't exist.
-if not os.path.isdir(validation_results_dir):
-    os.makedirs(validation_results_dir)
 
 class ImgListener(object):
 
@@ -40,26 +20,57 @@ class ImgListener(object):
         self.imageFrame = frame
         self.positionsL = []
         self.real_positionsL = []
-        #self.imageNumber = imageNumber
+
         cv2.namedWindow('image')
         cv2.setMouseCallback('image', self.listen_for_click, param=2)
 
     def listen_for_click(self, event, x, y, flags, param):
-        
-        #print param
+        """
+        This method is the event listener for the position clicked by user.
+        :param event: Type of event.
+        :param x: x-coordinate clicked by user.
+        :param y: y-coordinate clicked by user.
+        :param flags: -
+        :param param: -
+        :return: None
+        """
+
         if event == cv2.EVENT_LBUTTONUP and len(self.positionsL) < param:
             print x, y
             self.positionsL.append([x,y])
-            print "Now positionsL is,", self.positionsL
+
             coordinates = list(np.array([x, y])/2)
             self.real_positionsL.append(coordinates)
-            print "Now positions after halving is,", self.real_positionsL
+
 
     def show(self):
+        """
+        This method displays the current frame to the user.
+        :return: None
+        """
         cv2.imshow("image", self.imageFrame)
 
 
 if __name__ == "__main__":
+
+    ee = execfile
+    d = docopt.docopt(__doc__)
+    results_directory = os.path.join(configurations.output_dir, d['<results_directory>'])
+
+    if not os.path.exists(results_directory):
+        raise EnvironmentError("The '%s' does not exist. It probably means you have not run the analysis yet" % results_directory )
+
+
+    dir_path = os.path.join(results_directory, configurations.raw_imgs_dir)
+    results_file = "position_validation.csv"
+
+
+    validation_results_dir = os.path.join(results_directory, configurations.validation_results_dir)
+    fileToWriteResults = os.path.join(validation_results_dir, results_file)
+
+    # create directory if doesn't exist.
+    if not os.path.isdir(validation_results_dir):
+        os.makedirs(validation_results_dir)
 
     counter = 0
     frame_interval = 150
@@ -71,8 +82,8 @@ if __name__ == "__main__":
         image_path = os.path.join(dir_path, img_nameS)
 
         if os.path.exists(image_path):
-            print 'image path is', image_path
-            print 'reading image now'
+            print 'Image path is', image_path
+            print 'Reading image now'
             frame = cv2.imread(image_path)
         else:
             print img_nameS, 'does not exist'
@@ -81,13 +92,12 @@ if __name__ == "__main__":
         listener = ImgListener(frame)
 
         # showing the first image and waiting for 2 clicks
-        print 'showing image now'
+        print 'Showing image now'
         listener.show()
 
 
-
-        print "press 's' to skip to next image"
-        print "press 'a' once you are done clicking both flies"
+        print "Press 's' to skip to next image"
+        print "Press 'a' once you are done clicking both flies"
         print "You need to click the number 1 fly first."
         while True:
 
